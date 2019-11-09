884e9b52789035b56cc80ec0f6b0afe52d23c71b
rootVIII <colleyloyejames@gmail.com>
2019-11-09 16:14:46 -0500

    1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="robocall101",
-    version="1.1",
+    version="1.2",
     packages=["robocall101"],
     url="https://github.com/rootVIII/robocall101",
     license="MIT",
